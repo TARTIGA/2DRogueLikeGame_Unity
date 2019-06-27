fb0555ff5b3db986bcafa02a167363986c84f664
TARTIGA <tima_ig@mail.ru>
2019-06-27 21:30:00 +0300

    Wall class

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

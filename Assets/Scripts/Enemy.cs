acec3d20069093199eb8ac3d6339f20d9d3ba30a
TARTIGA <tima_ig@mail.ru>
2019-06-29 19:06:10 +0300

    add Enemy sound

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Enemy : MovingObject
 {
+
     public int playerDamage;
 
+    public AudioClip enemyAttack1;
+    public AudioClip enemyAttack2;
     private Animator animator;
     private Transform target;
     private bool skipMove;
@@ -50,7 +53,8 @@ public class Enemy : MovingObject
     protected override void OnCantMove<T>(T component)
     {
         Player hitPlayer = component as Player;
-        animator.SetTrigger("enemyAttack");
         hitPlayer.LoseFood(playerDamage);
+        animator.SetTrigger("enemyAttack");
+        SoundManager.instance.RandomizeSfx(enemyAttack1, enemyAttack2);
     }
 }

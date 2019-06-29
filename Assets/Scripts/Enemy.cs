78f5016124ceecab3927d4821c031916599770db
TARTIGA <tima_ig@mail.ru>
2019-06-29 16:07:59 +0300

    Enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+
+    }
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+        base.AttemptMove<T>(xDir, yDir);
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {
+        int xDir = 0;
+        int yDir = 0;
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+        {
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Player hitPlayer = component as Player;
+        hitPlayer.LoseFood(playerDamage);
+    }
+}

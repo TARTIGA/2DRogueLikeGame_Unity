3385d0d5380ef8a16a5d95160fe28277405100cf
TARTIGA <tima_ig@mail.ru>
2019-06-29 21:00:58 +0300

    fix player Inputs swipe add apk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,6 +23,7 @@ public class Player : MovingObject
 
     private Animator animator;
     private int food;
+    private Vector2 touchOrigin = -Vector2.one;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -43,6 +44,8 @@ public class Player : MovingObject
         int horizontal = 0;
         int vertical = 0;
 
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
 
@@ -50,6 +53,32 @@ public class Player : MovingObject
         {
             vertical = 0;
         }
+
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
+#endif
 
         if (horizontal != 0 || vertical != 0)
         {
